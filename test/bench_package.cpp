4be5424febd65dfd651070eb01c783215c7d12d2
Lukas Burgholzer <lukas.burgholzer@jku.at>
2021-03-24 14:39:35 +0100

    ✅ add controlled operation benchmark
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- test/bench_package.cpp
+++ test/bench_package.cpp
@@ -92,3 +92,59 @@ static void BM_MakeSingleQubitGateDD_TargetBottom(benchmark::State& state) {
     }
 }
 BENCHMARK(BM_MakeSingleQubitGateDD_TargetBottom)->Unit(benchmark::kMicrosecond)->RangeMultiplier(2)->Range(16, 128);
+
+static void BM_MakeControlledQubitGateDD_ControlBottom_TargetTop(benchmark::State& state) {
+    unsigned short nqubits = state.range(0);
+    auto           dd      = std::make_unique<dd::Package>();
+    auto           Xmat    = std::array<dd::ComplexValue, 4>{{{0., 0.}, {1., 0.}, {1., 0.}, {0., 0.}}};
+    auto           line    = std::array<short, dd::MAXN>{};
+    for (auto _: state) {
+        line.fill(-1);
+        line[0] = 1;
+        line[nqubits - 1] = 2;
+        benchmark::DoNotOptimize(dd->makeGateDD(Xmat, nqubits, line));
+    }
+}
+BENCHMARK(BM_MakeControlledQubitGateDD_ControlBottom_TargetTop)->Unit(benchmark::kMicrosecond)->RangeMultiplier(2)->Range(16, 128);
+
+static void BM_MakeControlledQubitGateDD_ControlBottom_TargetMiddle(benchmark::State& state) {
+    unsigned short nqubits = state.range(0);
+    auto           dd      = std::make_unique<dd::Package>();
+    auto           Xmat    = std::array<dd::ComplexValue, 4>{{{0., 0.}, {1., 0.}, {1., 0.}, {0., 0.}}};
+    auto           line    = std::array<short, dd::MAXN>{};
+    for (auto _: state) {
+        line.fill(-1);
+        line[0] = 1;
+        line[nqubits / 2] = 2;
+        benchmark::DoNotOptimize(dd->makeGateDD(Xmat, nqubits, line));
+    }
+}
+BENCHMARK(BM_MakeControlledQubitGateDD_ControlBottom_TargetMiddle)->Unit(benchmark::kMicrosecond)->RangeMultiplier(2)->Range(16, 128);
+
+static void BM_MakeControlledQubitGateDD_ControlTop_TargetMiddle(benchmark::State& state) {
+    unsigned short nqubits = state.range(0);
+    auto           dd      = std::make_unique<dd::Package>();
+    auto           Xmat    = std::array<dd::ComplexValue, 4>{{{0., 0.}, {1., 0.}, {1., 0.}, {0., 0.}}};
+    auto           line    = std::array<short, dd::MAXN>{};
+    for (auto _: state) {
+        line.fill(-1);
+        line[nqubits - 1] = 1;
+        line[nqubits / 2] = 2;
+        benchmark::DoNotOptimize(dd->makeGateDD(Xmat, nqubits, line));
+    }
+}
+BENCHMARK(BM_MakeControlledQubitGateDD_ControlTop_TargetMiddle)->Unit(benchmark::kMicrosecond)->RangeMultiplier(2)->Range(16, 128);
+
+static void BM_MakeControlledQubitGateDD_ControlTop_TargetBottom(benchmark::State& state) {
+    unsigned short nqubits = state.range(0);
+    auto           dd      = std::make_unique<dd::Package>();
+    auto           Xmat    = std::array<dd::ComplexValue, 4>{{{0., 0.}, {1., 0.}, {1., 0.}, {0., 0.}}};
+    auto           line    = std::array<short, dd::MAXN>{};
+    for (auto _: state) {
+        line.fill(-1);
+        line[nqubits - 1] = 1;
+        line[0] = 2;
+        benchmark::DoNotOptimize(dd->makeGateDD(Xmat, nqubits, line));
+    }
+}
+BENCHMARK(BM_MakeControlledQubitGateDD_ControlTop_TargetBottom)->Unit(benchmark::kMicrosecond)->RangeMultiplier(2)->Range(16, 128);
