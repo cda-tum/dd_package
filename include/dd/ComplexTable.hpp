fa44a430055b89e4846c56d9e21d6d638b3ea969
Lukas Burgholzer <lukas.burgholzer@jku.at>
2021-04-23 10:58:15 +0200

    ⏪ revert changes made for reducing gcc compile time as they are no longer needed
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- include/dd/ComplexTable.hpp
+++ include/dd/ComplexTable.hpp
@@ -9,7 +9,9 @@
 #include "Definitions.hpp"
 
 #include <algorithm>
+#include <array>
 #include <cassert>
+#include <cmath>
 #include <cstdint>
 #include <cstdlib>
 #include <iostream>
@@ -334,12 +336,9 @@ namespace dd {
 
     private:
         using Bucket = Entry*;
-        /// gcc is having serious troubles compiling this using std::array (compilation times >15min).
-        /// std::vector shouldn't be any less efficient in our application scenario
-        /// TODO: revisit this in the future
-        using Table = std::vector<Bucket>;
+        using Table  = std::array<Bucket, NBUCKET>;
 
-        Table table{NBUCKET};
+        Table table{};
 
         // numerical tolerance to be used for floating point values
         static inline fp TOLERANCE = 1e-13;
--- include/dd/UniqueTable.hpp
+++ include/dd/UniqueTable.hpp
@@ -11,6 +11,7 @@
 #include "Edge.hpp"
 
 #include <algorithm>
+#include <array>
 #include <cassert>
 #include <cstddef>
 #include <cstdint>
@@ -321,14 +322,11 @@ namespace dd {
 
     private:
         using NodeBucket = Node*;
-        /// gcc is having serious troubles compiling this using std::array (compilation times >15min).
-        /// std::vector shouldn't be any less efficient in our application scenario
-        /// TODO: revisit this in the future
-        using Table = std::vector<NodeBucket>;
+        using Table      = std::array<NodeBucket, NBUCKET>;
 
         // unique tables (one per input variable)
         std::size_t        nvars = 0;
-        std::vector<Table> tables{nvars, std::vector<NodeBucket>{NBUCKET}};
+        std::vector<Table> tables{nvars};
 
         Node*                                available{};
         std::vector<std::vector<Node>>       chunks{};
