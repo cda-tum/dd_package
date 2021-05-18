48c0a15e578da064cbc39ea91ca6131e031d3926
Stefan Hillmich <stefan.hillmich@jku.at>
2021-05-18 17:07:27 +0200

    Fixed format and steamlined workflow
    - set shell: bash as default

--- include/dd/UniqueTable.hpp
+++ include/dd/UniqueTable.hpp
@@ -33,13 +33,13 @@ namespace dd {
     template<class Node, std::size_t NBUCKET = 32768, std::size_t INITIAL_ALLOCATION_SIZE = 2048, std::size_t GROWTH_FACTOR = 2, std::size_t INITIAL_GC_LIMIT = 131072>
     class UniqueTable {
     public:
-        explicit UniqueTable(std::size_t nvars):
-            nvars(nvars), chunkID(0), allocationSize(INITIAL_ALLOCATION_SIZE), gcLimit(INITIAL_GC_LIMIT) {
+        explicit UniqueTable(std::size_t nvars) :
+                nvars(nvars), chunkID(0), allocationSize(INITIAL_ALLOCATION_SIZE), gcLimit(INITIAL_GC_LIMIT) {
             // allocate first chunk of nodes
             chunks.emplace_back(allocationSize);
             allocations += allocationSize;
             allocationSize *= GROWTH_FACTOR;
-            chunkIt    = chunks[0].begin();
+            chunkIt = chunks[0].begin();
             chunkEndIt = chunks[0].end();
         }
 
@@ -55,7 +55,7 @@ namespace dd {
             activeNodeCount = std::accumulate(active.begin(), active.end(), 0UL);
         }
 
-        static std::size_t hash(const Node* p) {
+        static std::size_t hash(const Node *p) {
             std::size_t key = 0;
             for (std::size_t i = 0; i < p->e.size(); ++i) {
                 key = dd::combineHash(key, std::hash<Edge<Node>>{}(p->e[i]));
@@ -70,28 +70,33 @@ namespace dd {
 
         // access functions
         [[nodiscard]] std::size_t getNodeCount() const { return nodeCount; }
+
         [[nodiscard]] std::size_t getPeakNodeCount() const { return peakNodeCount; }
+
         [[nodiscard]] std::size_t getMaxActiveNodes() const { return maxActive; }
+
         [[nodiscard]] std::size_t getAllocations() const { return allocations; }
-        [[nodiscard]] float       getGrowthFactor() const { return GROWTH_FACTOR; }
-        [[nodiscard]] const auto& getTables() const { return tables; }
+
+        [[nodiscard]] float getGrowthFactor() const { return GROWTH_FACTOR; }
+
+        [[nodiscard]] const auto &getTables() const { return tables; }
 
         // lookup a node in the unique table for the appropriate variable; insert it, if it has not been found
         // NOTE: reference counting is to be adjusted by function invoking the table lookup and only normalized nodes shall be stored.
-        Edge<Node> lookup(const Edge<Node>& e, bool keepNode = false) {
+        Edge <Node> lookup(const Edge <Node> &e, bool keepNode = false) {
             // there are unique terminal nodes
             if (e.isTerminal())
                 return e;
 
             lookups++;
             const auto key = hash(e.p);
-            const auto v   = e.p->v;
+            const auto v = e.p->v;
 
             // successors of a node shall either have successive variable numbers or be terminals
-            for ([[maybe_unused]] const auto& edge: e.p->e)
+            for ([[maybe_unused]] const auto &edge: e.p->e)
                 assert(edge.p->v == v - 1 || edge.isTerminal());
 
-            Node* p = tables[v][key];
+            Node *p = tables[v][key];
             while (p != nullptr) {
                 if (e.p->e == p->e) {
                     // Match found
@@ -105,7 +110,7 @@ namespace dd {
                     assert(p->v == e.p->v);
 
                     // successors of a node shall either have successive variable numbers or be terminals
-                    for ([[maybe_unused]] const auto& edge: e.p->e)
+                    for ([[maybe_unused]] const auto &edge: e.p->e)
                         assert(edge.p->v == v - 1 || edge.isTerminal());
 
                     return {p, e.w};
@@ -115,7 +120,7 @@ namespace dd {
             }
 
             // node was not found -> add it to front of unique table bucket
-            e.p->next      = tables[v][key];
+            e.p->next = tables[v][key];
             tables[v][key] = e.p;
             nodeCount++;
             peakNodeCount = std::max(peakNodeCount, nodeCount);
@@ -123,10 +128,10 @@ namespace dd {
             return e;
         }
 
-        [[nodiscard]] Node* getNode() {
+        [[nodiscard]] Node *getNode() {
             // a node is available on the stack
             if (available != nullptr) {
-                Node* p   = available;
+                Node *p = available;
                 available = p->next;
                 // returned nodes could have a ref count != 0
                 p->ref = 0;
@@ -139,7 +144,7 @@ namespace dd {
                 allocations += allocationSize;
                 allocationSize *= GROWTH_FACTOR;
                 chunkID++;
-                chunkIt    = chunks[chunkID].begin();
+                chunkIt = chunks[chunkID].begin();
                 chunkEndIt = chunks[chunkID].end();
             }
 
@@ -148,28 +153,29 @@ namespace dd {
             return p;
         }
 
-        void returnNode(Node* p) {
-            p->next   = available;
+        void returnNode(Node *p) {
+            p->next = available;
             available = p;
         }
 
         // increment reference counter for node e points to
         // and recursively increment reference counter for
         // each child if this is the first reference
-        void incRef(const Edge<Node>& e) {
+        void incRef(const Edge <Node> &e) {
             dd::ComplexNumbers::incRef(e.w);
             if (e.p == nullptr || e.isTerminal())
                 return;
 
             if (e.p->ref == std::numeric_limits<decltype(e.p->ref)>::max()) {
-                std::clog << "[WARN] MAXREFCNT reached for p=" << reinterpret_cast<std::uintptr_t>(e.p) << ". Node will never be collected." << std::endl;
+                std::clog << "[WARN] MAXREFCNT reached for p=" << reinterpret_cast<std::uintptr_t>(e.p)
+                          << ". Node will never be collected." << std::endl;
                 return;
             }
 
             e.p->ref++;
 
             if (e.p->ref == 1) {
-                for (const auto& edge: e.p->e) {
+                for (const auto &edge: e.p->e) {
                     if (edge.p != nullptr) {
                         incRef(edge);
                     }
@@ -183,7 +189,7 @@ namespace dd {
         // decrement reference counter for node e points to
         // and recursively decrement reference counter for
         // each child if this is the last reference
-        void decRef(const Edge<Node>& e) {
+        void decRef(const Edge <Node> &e) {
             dd::ComplexNumbers::decRef(e.w);
             if (e.p == nullptr || e.isTerminal()) return;
             if (e.p->ref == std::numeric_limits<decltype(e.p->ref)>::max()) return;
@@ -195,7 +201,7 @@ namespace dd {
             e.p->ref--;
 
             if (e.p->ref == 0) {
-                for (const auto& edge: e.p->e) {
+                for (const auto &edge: e.p->e) {
                     if (edge.p != nullptr) {
                         decRef(edge);
                     }
@@ -215,14 +221,14 @@ namespace dd {
             gcRuns++;
             std::size_t collected = 0;
             std::size_t remaining = 0;
-            for (auto& table: tables) {
-                for (auto& bucket: table) {
-                    Node* p     = bucket;
-                    Node* lastp = nullptr;
+            for (auto &table: tables) {
+                for (auto &bucket: table) {
+                    Node *p = bucket;
+                    Node *lastp = nullptr;
                     while (p != nullptr) {
                         if (p->ref == 0) {
                             assert(!Node::isTerminal(p));
-                            Node* next = p->next;
+                            Node *next = p->next;
                             if (lastp == nullptr) {
                                 bucket = next;
                             } else {
@@ -233,7 +239,7 @@ namespace dd {
                             collected++;
                         } else {
                             lastp = p;
-                            p     = p->next;
+                            p = p->next;
                             remaining++;
                         }
                     }
@@ -255,8 +261,8 @@ namespace dd {
 
         void clear() {
             // clear unique table buckets
-            for (auto& table: tables) {
-                for (auto& bucket: table) {
+            for (auto &table: tables) {
+                for (auto &bucket: table) {
                     bucket = nullptr;
                 }
             }
@@ -269,35 +275,35 @@ namespace dd {
                 chunkID--;
             }
             // restore initial chunk setting
-            chunkIt        = chunks[0].begin();
-            chunkEndIt     = chunks[0].end();
+            chunkIt = chunks[0].begin();
+            chunkEndIt = chunks[0].end();
             allocationSize = INITIAL_ALLOCATION_SIZE * GROWTH_FACTOR;
-            allocations    = INITIAL_ALLOCATION_SIZE;
+            allocations = INITIAL_ALLOCATION_SIZE;
 
-            for (auto& node: chunks[0]) {
+            for (auto &node: chunks[0]) {
                 node.ref = 0;
             }
 
-            nodeCount     = 0;
+            nodeCount = 0;
             peakNodeCount = 0;
 
             collisions = 0;
-            hits       = 0;
-            lookups    = 0;
+            hits = 0;
+            lookups = 0;
 
             std::fill(active.begin(), active.end(), 0);
             activeNodeCount = 0;
-            maxActive       = 0;
+            maxActive = 0;
 
             gcCalls = 0;
-            gcRuns  = 0;
+            gcRuns = 0;
             gcLimit = INITIAL_GC_LIMIT;
         };
 
         void print() {
             Qubit q = nvars - 1;
             for (auto it = tables.rbegin(); it != tables.rend(); ++it) {
-                auto& table = *it;
+                auto &table = *it;
                 std::cout << "\tq" << static_cast<std::size_t>(q) << ":"
                           << "\n";
                 for (std::size_t key = 0; key < table.size(); ++key) {
@@ -306,9 +312,12 @@ namespace dd {
                         std::cout << "\tkey=" << key << ": ";
 
                     while (p != nullptr) {
-                        std::cout << "\t\t" << std::hex << reinterpret_cast<std::uintptr_t>(p) << std::dec << " " << p->ref << std::hex;
-                        for (const auto& e: p->e) {
-                            std::cout << " p" << reinterpret_cast<std::uintptr_t>(e.p) << "(r" << reinterpret_cast<std::uintptr_t>(e.w.r) << " i" << reinterpret_cast<std::uintptr_t>(e.w.i) << ")";
+                        std::cout << "\t\t" << std::hex << reinterpret_cast<std::uintptr_t>(p) << std::dec << " "
+                                  << p->ref << std::hex;
+                        for (const auto &e: p->e) {
+                            std::cout << " p" << reinterpret_cast<std::uintptr_t>(e.p) << "(r"
+                                      << reinterpret_cast<std::uintptr_t>(e.w.r) << " i"
+                                      << reinterpret_cast<std::uintptr_t>(e.w.i) << ")";
                         }
                         std::cout << std::dec << "\n";
                         p = p->next;
@@ -320,57 +329,61 @@ namespace dd {
 
         void printActive() {
             std::cout << "#printActive: " << activeNodeCount << ", ";
-            for (const auto& a: active)
+            for (const auto &a: active)
                 std::cout << a << " ";
             std::cout << "\n";
         }
 
         [[nodiscard]] fp hitRatio() const { return static_cast<fp>(hits) / lookups; }
+
         [[nodiscard]] fp colRatio() const { return static_cast<fp>(collisions) / lookups; }
 
         [[nodiscard]] std::size_t getActiveNodeCount() const {
             return activeNodeCount;
         }
+
         [[nodiscard]] std::size_t getActiveNodeCount(Qubit var) { return active.at(var); }
 
-        std::ostream& printStatistics(std::ostream& os = std::cout) {
-            os << "hits: " << hits << ", collisions: " << collisions << ", looks: " << lookups << ", hitRatio: " << hitRatio() << ", colRatio: " << colRatio() << ", gc calls: " << gcCalls << ", gc runs: " << gcRuns << "\n";
+        std::ostream &printStatistics(std::ostream &os = std::cout) {
+            os << "hits: " << hits << ", collisions: " << collisions << ", looks: " << lookups << ", hitRatio: "
+               << hitRatio() << ", colRatio: " << colRatio() << ", gc calls: " << gcCalls << ", gc runs: " << gcRuns
+               << "\n";
             return os;
         }
 
     private:
-        using NodeBucket = Node*;
-        using Table      = std::array<NodeBucket, NBUCKET>;
+        using NodeBucket = Node *;
+        using Table = std::array<NodeBucket, NBUCKET>;
 
         // unique tables (one per input variable)
-        std::size_t        nvars = 0;
+        std::size_t nvars = 0;
         std::vector<Table> tables{nvars};
 
-        Node*                                available{};
-        std::vector<std::vector<Node>>       chunks{};
-        std::size_t                          chunkID;
+        Node *available{};
+        std::vector<std::vector<Node>> chunks{};
+        std::size_t chunkID;
         typename std::vector<Node>::iterator chunkIt;
         typename std::vector<Node>::iterator chunkEndIt;
-        std::size_t                          allocationSize;
+        std::size_t allocationSize;
 
-        std::size_t allocations   = 0;
-        std::size_t nodeCount     = 0;
+        std::size_t allocations = 0;
+        std::size_t nodeCount = 0;
         std::size_t peakNodeCount = 0;
 
         // unique table lookup statistics
         std::size_t collisions = 0;
-        std::size_t hits       = 0;
-        std::size_t lookups    = 0;
+        std::size_t hits = 0;
+        std::size_t lookups = 0;
 
         // (max) active nodes
         // number of active vector nodes for each variable
         std::vector<std::size_t> active{std::vector<std::size_t>(nvars, 0)};
-        std::size_t              activeNodeCount = 0;
-        std::size_t              maxActive       = 0;
+        std::size_t activeNodeCount = 0;
+        std::size_t maxActive = 0;
 
         // garbage collection
         std::size_t gcCalls = 0;
-        std::size_t gcRuns  = 0;
+        std::size_t gcRuns = 0;
         std::size_t gcLimit = 250000;
     };
 
