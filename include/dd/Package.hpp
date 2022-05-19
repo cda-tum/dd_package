43069d1826c2baf64137f36c28916635b6c252f6
Thomas <Thomas.Grurl@fh-hagenberg.at>
2022-05-19 15:45:16 +0200

    added a simple bugfix for the limvector bug

--- include/dd/Package.hpp
+++ include/dd/Package.hpp
@@ -739,12 +739,14 @@ namespace dd {
                 e = normalizeLIMDD(e, cached);
                 assert(e.p->v == var || e.isTerminal());
 
+                e.p->limVector = Pauli::constructStabilizerGeneratorSetZ(*(e.p));
+
                 // look it up in the unique tables
                 auto l = uniqueTable.lookup(e, false);
                 assert(l.p->v == var || l.isTerminal());
                 // TODO skip constructing the stabilizer generator set if it has already been found,
                 //   i.e., only compute the group once, when the node is allocated; and not when the node lookup was succesful
-                l.p->limVector = Pauli::constructStabilizerGeneratorSetZ(*(l.p));
+//                l.p->limVector = Pauli::constructStabilizerGeneratorSetZ(*(l.p));
                 std::cout << "[makeDDNode] constructed Stabgenset:\n";
                 std::cout.flush();
                 Pauli::printStabilizerGroup(l.p->limVector);
