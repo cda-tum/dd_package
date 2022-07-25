815a28530dc3bbaa9d0bbe1b493464a6dc8c8580
Thomas Grurl <thomas.grurl@outlook.com>
2022-07-25 22:07:41 +0200

    inline functions and reformat code

--- include/dd/CVecUtilities.hpp
+++ include/dd/CVecUtilities.hpp
@@ -1,70 +1,70 @@
-#ifndef CVEC_UTITITIES_HPP
-#define CVEC_UTITITIES_HPP
+#pragma once
 
-#include "Definitions.hpp"
-#include "Log.hpp"
+#ifndef CVEC_UTITITIES_HPP
+    #define CVEC_UTITITIES_HPP
+
+    #include "Definitions.hpp"
+    #include "Log.hpp"
 
 namespace dd {
 
-
-_Log& outputCVec(const CVec& vec) {
-	Log::log << "[";
-	for (unsigned int i = 0; i < vec.size(); i++) {
-		Log::log << vec[i] << ", ";
-	}
-	return Log::log << "]";
-}
-
-void printCVec(const std::vector<std::complex<fp>>& vec) {
-    Log::log << outputCVec(vec);
-}
-
-bool isZeroVector(const CVec& vec) {
-    for (unsigned int i = 0; i < vec.size(); i++) {
-        if (!Complex::approximatelyEqual(vec[i], 0)) {
-            return false;
+    inline _Log& outputCVec(const CVec& vec) {
+        Log::log << "[";
+        for (unsigned int i = 0; i < vec.size(); i++) {
+            Log::log << vec[i] << ", ";
         }
+        return Log::log << "]";
     }
-    return true;
-}
 
-bool isZeroVector(const CVec& vec, unsigned int start, unsigned int end) {
-    if (vec.size() >= start || vec.size() >= end) {
-        throw std::runtime_error("[isZeroVector] ERROR received start and end which are out of bounds.");
+    inline void printCVec(const std::vector<std::complex<fp>>& vec) {
+        Log::log << outputCVec(vec);
     }
-    for (unsigned int i = start; i < end; i++) {
-        if (!Complex::approximatelyEqual(vec[i], 0)) {
-            return false;
+
+    inline bool isZeroVector(const CVec& vec) {
+        for (unsigned int i = 0; i < vec.size(); i++) {
+            if (!Complex::approximatelyEqual(vec[i], 0)) {
+                return false;
+            }
         }
+        return true;
     }
-    return true;
-}
 
-CVec addVectors(const CVec a, const CVec b) {
-    unsigned int N = a.size();
-    CVec         c(N, {0.0, 0.0});
-    for (unsigned int i = 0; i < N; i++) {
-        c[i] = a[i] + b[i];
+    inline bool isZeroVector(const CVec& vec, unsigned int start, unsigned int end) {
+        if (vec.size() >= start || vec.size() >= end) {
+            throw std::runtime_error("[isZeroVector] ERROR received start and end which are out of bounds.");
+        }
+        for (unsigned int i = start; i < end; i++) {
+            if (!Complex::approximatelyEqual(vec[i], 0)) {
+                return false;
+            }
+        }
+        return true;
     }
-    return c;
-}
 
-CVec multiplyMatrixVector(const CMat mat, const CVec x) {
-    unsigned int N = std::max(mat.size(), x.size());
-    CVec         y(N, {0.0, 0.0});
-    if (mat.size() != x.size()) {
+    inline CVec addVectors(const CVec a, const CVec b) {
+        unsigned int N = a.size();
+        CVec         c(N, {0.0, 0.0});
+        for (unsigned int i = 0; i < N; i++) {
+            c[i] = a[i] + b[i];
+        }
+        return c;
+    }
+
+    inline CVec multiplyMatrixVector(const CMat mat, const CVec x) {
+        unsigned int N = std::max(mat.size(), x.size());
+        CVec         y(N, {0.0, 0.0});
+        if (mat.size() != x.size()) {
+            return y;
+        }
+        for (unsigned int row = 0; row < N; row++) {
+            for (unsigned int col = 0; col < N; col++) {
+                //        			y[row] += x[col] * mat[col][row];
+                y[row] += x[col] * mat[row][col]; // Or is this the right order?
+            }
+        }
         return y;
     }
-    for (unsigned int row = 0; row < N; row++) {
-        for (unsigned int col = 0; col < N; col++) {
-            //        			y[row] += x[col] * mat[col][row];
-            y[row] += x[col] * mat[row][col]; // Or is this the right order?
-        }
-    }
-    return y;
-}
 
-}
-
+} // namespace dd
 
 #endif
